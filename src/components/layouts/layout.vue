7d65ede6995e406a0ee2a3cfc30da1719b7d678f
Vannysa23 <vannysa.999967@gmail.com>
2025-07-07 22:05:53 +0700

    Vue components

---
+++ src/components/layouts/layout.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <h1>Header</h1>
+    </header>
+    <main>
+        <slot />
+    </main>
+    <footer>
+        <small>Vue Learning Journey</small>
+    </footer>
+</template>
+
+<style scoped>
+ 
+</style>
